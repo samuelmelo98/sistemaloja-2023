68a9c8bd6ac6d0d1f0b0236423097b9c2b0c7439
samuelmelo98 <samuelmelo98@hotmail.com>
2022-12-19 13:19:29 -0200

    Criação de filter

--- src/main/java/teste/controle/UsuarioSistemaLoginBean.java
+++ src/main/java/teste/controle/UsuarioSistemaLoginBean.java
@@ -9,24 +9,27 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 
 import lombok.Data;
+import teste.model.Notificacao;
 import teste.model.UsuarioSistemaLogin;
 import teste.service.UsuarioSistemaLoginService;
+
 @Named
 @Data
 @SessionScoped
-public class UsuarioSistemaLoginBean implements Serializable{
+public class UsuarioSistemaLoginBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private UsuarioSistemaLogin usuariosistemalogin;
 	@Inject
 	private UsuarioSistemaLoginService usuariosistemaloginservice;
 	private List<UsuarioSistemaLogin> listaUsuarios;
+	private boolean verifica = false;
 
-	
 	public void adicionarUsuario() {
 		listaUsuarios = new ArrayList<UsuarioSistemaLogin>();
 		listaUsuarios.add(usuariosistemalogin);
@@ -35,7 +38,7 @@ public class UsuarioSistemaLoginBean implements Serializable{
 		String senha = usuariosistemalogin.getUsuarioSenha().trim();
 		usuariosistemalogin.setUsuarioNome(nome);
 		usuariosistemalogin.setUsuarioNome(senha);
-		
+
 		if (usuariosistemaloginservice.adicionarUsuario(usuariosistemalogin)) {
 			FacesMessage msg = new FacesMessage("Sucesso!", "Usuário foi Cadastrado!");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
@@ -46,36 +49,87 @@ public class UsuarioSistemaLoginBean implements Serializable{
 		}
 
 	}
-	
-	public void validarUsuario() {
+
+	public String validarUsuario() {
 		Boolean control = true;
-		listaUsuarios =new ArrayList<UsuarioSistemaLogin>();
+		listaUsuarios = new ArrayList<UsuarioSistemaLogin>();
 		listaUsuarios = usuariosistemaloginservice.pequisarUsuario();
-		for(UsuarioSistemaLogin x:listaUsuarios) {
-			if(x.getUsuarioNome().equals(usuariosistemalogin.getUsuarioNome())&& x.getUsuarioSenha().equals(usuariosistemalogin.getUsuarioSenha())) {
-			System.out.println("Achou");
-			FacesMessage msg = new FacesMessage("Bem Vindo!", usuariosistemalogin.getUsuarioNome());
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			control = false;
-			break;
-			
-			}
-		}
-			if(control) {
-				FacesMessage msg = new FacesMessage("Erro!", "Usuário não encontrado!");
+		for (UsuarioSistemaLogin x : listaUsuarios) {
+			if (x.getUsuarioNome().equals(usuariosistemalogin.getUsuarioNome())
+					&& x.getUsuarioSenha().equals(usuariosistemalogin.getUsuarioSenha())) {
+				System.out.println("Achou");
+				FacesMessage msg = new FacesMessage("Bem Vindo!", usuariosistemalogin.getUsuarioNome());
 				FacesContext.getCurrentInstance().addMessage(null, msg);
+				control = false;
+				HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+						.getSession(false);
+				session.setAttribute("usuario", usuariosistemalogin);
+				break;
+				// return "/index?faces-redirect=true";
+
 			}
-			
-		
-		
+
+		}
+		if (control) {
+			FacesMessage msg = new FacesMessage("Erro!", "Usuário não encontrado!");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+
+			// FacesContext.getCurrentInstance().getExternalContext().redirect("login.xhtml");
+		}
+		// return "/login/login?faces-redirect=true";
+		return null;
+
+		// return null;
 	}
-		
-	
-	
+	// return "/index?faces-redirect=true";
+
 	public void limpar() {
 		usuariosistemalogin = new UsuarioSistemaLogin();
-		
+
 		System.out.println("ok");
 	}
+
+	public String passarUsuario() {
+		String user = usuariosistemalogin.getUsuarioNome();
+		return user;
+	}
 	
+	public String validarUsuario2() {
+		
+		listaUsuarios = new ArrayList<UsuarioSistemaLogin>();
+		listaUsuarios = usuariosistemaloginservice.pequisarUsuario();
+		for (UsuarioSistemaLogin x : listaUsuarios) {
+			if (x.getUsuarioNome().equals(usuariosistemalogin.getUsuarioNome())
+					&& x.getUsuarioSenha().equals(usuariosistemalogin.getUsuarioSenha())) {
+				FacesMessage msg = new FacesMessage("Bem Vindo!", usuariosistemalogin.getUsuarioNome());
+				FacesContext.getCurrentInstance().addMessage(null, msg);
+				HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+				verifica = true;
+				break;
+			}
+
+		}
+		if (verifica) {
+			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+					.getSession(false);
+			session.setAttribute("usuario", usuariosistemalogin);// atributo para sessão
+			verifica = false;
+			return "/index?faces-redirect=true";
+		} else {
+			FacesMessage msg = new FacesMessage("Acesso Negado!", usuariosistemalogin.getUsuarioNome() + "Usuário não encontrado!");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			Notificacao.msgAviso("Acesso Negado", "Usuário não encontrado!");
+			// HttpSession session = (HttpSession)
+			// FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+			// session.setAttribute("usuario", usuariosistemalogin);
+			return "/login/login?faces-redirect=true";
+		}
+
+	}
+
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/login/login?faces-redirect=true";
+	}
 }
